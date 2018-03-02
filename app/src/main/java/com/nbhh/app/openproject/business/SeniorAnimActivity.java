5f1ecfb189958474b629d2b61d06a8338bec9e84
wlq <790982813@qq.com>
2018-03-02 11:50:42 +0800

    1、矢量动画例子一个；

--- app/src/main/java/com/nbhh/app/openproject/business/SeniorAnimActivity.java
+++ app/src/main/java/com/nbhh/app/openproject/business/SeniorAnimActivity.java
@@ -1,8 +1,12 @@
 package com.nbhh.app.openproject.business;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.nbhh.app.openproject.R;
 
@@ -11,12 +15,27 @@ import com.nbhh.app.openproject.R;
  * @description 高级动画
  * @date 2018/3/1 下午6:01
  */
-public class SeniorAnimActivity extends AppCompatActivity {
+public class SeniorAnimActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView ivThreeLineAnim;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_senior_anim);
+
+        ivThreeLineAnim = (ImageView) findViewById(R.id.iv_three_line_anim);
+        ivThreeLineAnim.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_three_line_anim:
+                Drawable drawable = ivThreeLineAnim.getDrawable();
+                ((Animatable)drawable).start();
+                break;
+        }
     }
 }
